e56e97d2f07deb151c390a8115f7da424318dada
Alvin Kim <alvink@gmail.com>
2020-01-21 12:59:00 -0800

    Updates

---
+++ precache-manifest.922aa517abbf9392a67446cb0de1fd75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "963901eea2e6b79f0e58",
+    "url": "/tfm-randomizer/static/js/runtime~main.092d6da5.js"
+  },
+  {
+    "revision": "39b15c0c678c17f59ac1",
+    "url": "/tfm-randomizer/static/js/main.8f51d199.chunk.js"
+  },
+  {
+    "revision": "7f138694aebf8428889b",
+    "url": "/tfm-randomizer/static/js/2.0b84c11a.chunk.js"
+  },
+  {
+    "revision": "39b15c0c678c17f59ac1",
+    "url": "/tfm-randomizer/static/css/main.345ea129.chunk.css"
+  },
+  {
+    "revision": "f57753332cfe03368ae5cbbe604191b5",
+    "url": "/tfm-randomizer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7e843885c5187c803c6cf8a45a995725.js"
+  "/tfm-randomizer/precache-manifest.922aa517abbf9392a67446cb0de1fd75.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/tfm-randomizer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
